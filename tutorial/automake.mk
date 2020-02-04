a9936721d906b61f586c21d5da40bfb6b7b9e0e2
Nic30 <nic30@seznam.cz>
2020-02-04 16:52:04 +0100

    pcv tutorials: add to distrib. files

--- tutorial/automake.mk
+++ tutorial/automake.mk
@@ -5,7 +5,12 @@ EXTRA_DIST += \
 	tutorial/t-stage1 \
 	tutorial/t-stage2 \
 	tutorial/t-stage3 \
-	tutorial/t-stage4
+	tutorial/t-stage4 \
+	tutorial/ovn-setup.sh \
+        tutorial/ovs_pcv/ovs_dpdk_pcv.sh \
+        tutorial/ovs_pcv/ovs_reboot.sh \
+        tutorial/ovs_pcv/ovs_test_iperf.sh
+
 sandbox: all
 	cd $(srcdir)/tutorial && MAKE=$(MAKE) HAVE_OPENSSL=$(HAVE_OPENSSL) \
 		./ovs-sandbox -b $(abs_builddir) $(SANDBOXFLAGS)
